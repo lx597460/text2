72a2013964c3ecf6f18a64b4e24da5f18ee1e915
lx597460 <lx597460@gmail.com>
2026-04-25 20:39:44 +0800

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -1,4 +1,4 @@
-package states;   包;
+package states;
 
 import backend.WeekData;
 import backend.Highscore;
@@ -13,7 +13,6 @@ import substates.ResetScoreSubState;
 import flixel.math.FlxMath;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
-import flixel.util.FlxTimer;
 
 class FreeplayState extends MusicBeatState
 {
@@ -50,11 +49,6 @@ class FreeplayState extends MusicBeatState
 	var bottomBG:FlxSprite;
 
 	var player:MusicPlayer;
-	
-	var blackBarsLeft:FlxSprite;
-	var blackBarsRight:FlxSprite;
-	var zoomCamera:Bool = false;
-	var zoomAmount:Float = 1;
 
 	override function create()
 	{
@@ -102,9 +96,8 @@ class FreeplayState extends MusicBeatState
 
 		for (i in 0...songs.length)
 		{
-			var songText:Alphabet = new Alphabet(0, 0, songs[i].songName, true);
+			var songText:Alphabet = new Alphabet(90, 320, songs[i].songName, true);
 			songText.targetY = i;
-			songText.screenCenter(X);
 			grpSongs.add(songText);
 
 			songText.scaleX = Math.min(1, 980 / songText.width);
@@ -113,7 +106,6 @@ class FreeplayState extends MusicBeatState
 			Mods.currentModDirectory = songs[i].folder;
 			var icon:HealthIcon = new HealthIcon(songs[i].songCharacter);
 			icon.sprTracker = songText;
-			icon.y = songText.y + 60;
 			
 			songText.visible = songText.active = songText.isMenuItem = false;
 			icon.visible = icon.active = false;
@@ -123,17 +115,18 @@ class FreeplayState extends MusicBeatState
 		}
 		WeekData.setDirectoryFromWeek();
 
-		scoreText = new FlxText(0, 10, FlxG.width, "", 32);
-		scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
-		scoreText.y = 10;
+		scoreText = new FlxText(FlxG.width * 0.7, 5, 0, "", 32);
+		scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
 
-		scoreBG = new FlxSprite(0, 0).makeGraphic(FlxG.width, 50, 0xFF000000);
+		scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 66, 0xFF000000);
 		scoreBG.alpha = 0.6;
 		add(scoreBG);
-		add(scoreText);
 
-		diffText = new FlxText(0, 0, 0, "", 24);
-		diffText.visible = false;
+		diffText = new FlxText(scoreText.x, scoreText.y + 36, 0, "", 24);
+		diffText.font = scoreText.font;
+		add(diffText);
+
+		add(scoreText);
 
 		missingTextBG = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		missingTextBG.alpha = 0.6;
@@ -157,7 +150,13 @@ class FreeplayState extends MusicBeatState
 		bottomBG.alpha = 0.6;
 		add(bottomBG);
 
-		var leText:String = "Press SPACE to listen / Press RESET to reset score";
+        var leText:String;
+
+        if (controls.mobileC) {
+			leText = "Press X to listen to the Song / Press Y to Reset your Score and Accuracy.";
+        } else {
+			leText = "Press SPACE to listen to the Song / Press RESET to Reset your Score and Accuracy.";
+        }
 		
 		bottomString = leText;
 		var size:Int = 16;
@@ -168,13 +167,6 @@ class FreeplayState extends MusicBeatState
 		
 		player = new MusicPlayer(this);
 		add(player);
-		
-		blackBarsLeft = new FlxSprite(-FlxG.width, 0).makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		blackBarsRight = new FlxSprite(FlxG.width, 0).makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		blackBarsLeft.alpha = 0;
-		blackBarsRight.alpha = 0;
-		add(blackBarsLeft);
-		add(blackBarsRight);
 		
 		changeSelection();
 		updateTexts();
@@ -213,13 +205,6 @@ class FreeplayState extends MusicBeatState
 	
 	override function update(elapsed:Float)
 	{
-		if (zoomCamera)
-		{
-			zoomAmount += elapsed * 2;
-			FlxG.camera.zoom = zoomAmount;
-			if (zoomAmount >= 2) zoomAmount = 2;
-		}
-		
 		if (FlxG.sound.music.volume < 0.7)
 		{
 			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
@@ -249,9 +234,10 @@ class FreeplayState extends MusicBeatState
 		if(FlxG.keys.pressed.SHIFT && !player.playingMusic) shiftMult = 3;
 		#end
 
-		if (!player.playingMusic && !zoomCamera)
+		if (!player.playingMusic)
 		{
 			scoreText.text = 'PERSONAL BEST: ' + lerpScore + ' (' + ratingSplit.join('.') + '%)';
+			positionHighscore();
 			
 			if(songs.length > 1)
 			{
@@ -297,7 +283,7 @@ class FreeplayState extends MusicBeatState
 			}
 		}
 
-		if (controls.BACK && !zoomCamera)
+		if (controls.BACK)
 		{
 			if (player.playingMusic)
 			{
@@ -324,7 +310,7 @@ class FreeplayState extends MusicBeatState
 		}
 
 		#if mobile
-		if((FlxG.keys.justPressed.CONTROL || touchPad.buttonC.justPressed) && !player.playingMusic && !zoomCamera)
+		if((FlxG.keys.justPressed.CONTROL || touchPad.buttonC.justPressed) && !player.playingMusic)
 		{
 			persistentUpdate = false;
 			openSubState(new GameplayChangersSubstate());
@@ -332,7 +318,7 @@ class FreeplayState extends MusicBeatState
 		}
 		else if(FlxG.keys.justPressed.SPACE || touchPad.buttonX.justPressed)
 		{
-			if(instPlaying != curSelected && !player.playingMusic && !zoomCamera)
+			if(instPlaying != curSelected && !player.playingMusic)
 			{
 				destroyFreeplayVocals();
 				FlxG.sound.music.volume = 0;
@@ -371,7 +357,7 @@ class FreeplayState extends MusicBeatState
 				player.pauseOrResume(player.paused);
 			}
 		}
-		else if (controls.ACCEPT && !player.playingMusic && !zoomCamera)
+		else if (controls.ACCEPT && !player.playingMusic)
 		{
 			FlxG.sound.play(Paths.sound('confirmMenu'));
 			
@@ -410,7 +396,7 @@ class FreeplayState extends MusicBeatState
 				return;
 			}
 		}
-		else if((controls.RESET || touchPad.buttonY.justPressed) && !player.playingMusic && !zoomCamera)
+		else if((controls.RESET || touchPad.buttonY.justPressed) && !player.playingMusic)
 		{
 			persistentUpdate = false;
 			openSubState(new ResetScoreSubState(songs[curSelected].songName, curDifficulty, songs[curSelected].songCharacter));
@@ -418,14 +404,14 @@ class FreeplayState extends MusicBeatState
 			FlxG.sound.play(Paths.sound('scrollMenu'));
 		}
 		#else
-		if((FlxG.keys.justPressed.CONTROL) && !player.playingMusic && !zoomCamera)
+		if((FlxG.keys.justPressed.CONTROL) && !player.playingMusic)
 		{
 			persistentUpdate = false;
 			openSubState(new GameplayChangersSubstate());
 		}
 		else if(FlxG.keys.justPressed.SPACE)
 		{
-			if(instPlaying != curSelected && !player.playingMusic && !zoomCamera)
+			if(instPlaying != curSelected && !player.playingMusic)
 			{
 				destroyFreeplayVocals();
 				FlxG.sound.music.volume = 0;
@@ -464,7 +450,7 @@ class FreeplayState extends MusicBeatState
 				player.pauseOrResume(player.paused);
 			}
 		}
-		else if (controls.ACCEPT && !player.playingMusic && !zoomCamera)
+		else if (controls.ACCEPT && !player.playingMusic)
 		{
 			FlxG.sound.play(Paths.sound('confirmMenu'));
 			
@@ -503,7 +489,7 @@ class FreeplayState extends MusicBeatState
 				return;
 			}
 		}
-		else if((controls.RESET) && !player.playingMusic && !zoomCamera)
+		else if((controls.RESET) && !player.playingMusic)
 		{
 			persistentUpdate = false;
 			openSubState(new ResetScoreSubState(songs[curSelected].songName, curDifficulty, songs[curSelected].songCharacter));
@@ -517,27 +503,19 @@ class FreeplayState extends MusicBeatState
 
 	function startTransitionToGame()
 	{
-		var centerY:Float = FlxG.height / 2;
+		var whiteScreen:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
+		whiteScreen.alpha = 0;
+		whiteScreen.scrollFactor.set();
+		add(whiteScreen);
 		
 		for (item in grpSongs.members)
 		{
 			if (item != null && item.visible && item.exists)
 			{
-				var targetY:Float = centerY + ((item.targetY - curSelected) * 80);
-				
-				if (item.targetY < curSelected)
-				{
-					FlxTween.tween(item, {y: targetY - 150, alpha: 0}, 0.5, {ease: FlxEase.quartIn});
-				}
-				else if (item.targetY > curSelected)
-				{
-					FlxTween.tween(item, {y: targetY + 150, alpha: 0}, 0.5, {ease: FlxEase.quartIn});
-				}
-				else
-				{
-					FlxTween.tween(item.scale, {x: 1.2, y: 1.2}, 0.3, {ease: FlxEase.quartOut});
-					FlxTween.tween(item, {alpha: 0}, 0.5, {ease: FlxEase.quartIn, startDelay: 0.2});
-				}
+				FlxTween.tween(item, {alpha: 0}, 0.4, {
+					ease: FlxEase.quartOut,
+					startDelay: 0.1
+				});
 			}
 		}
 		
@@ -549,29 +527,69 @@ class FreeplayState extends MusicBeatState
 			}
 		}
 		
-		FlxTween.tween(scoreText, {alpha: 0}, 0.3);
-		FlxTween.tween(scoreBG, {alpha: 0}, 0.3);
-		FlxTween.tween(bottomText, {alpha: 0}, 0.3);
-		FlxTween.tween(bottomBG, {alpha: 0}, 0.3);
+		if (scoreText != null && scoreText.exists) FlxTween.tween(scoreText, {alpha: 0}, 0.3);
+		if (diffText != null && diffText.exists) FlxTween.tween(diffText, {alpha: 0}, 0.3);
+		if (scoreBG != null && scoreBG.exists) FlxTween.tween(scoreBG, {alpha: 0}, 0.3);
+		if (bottomText != null && bottomText.exists) FlxTween.tween(bottomText, {alpha: 0}, 0.3);
+		if (bottomBG != null && bottomBG.exists) FlxTween.tween(bottomBG, {alpha: 0}, 0.3);
 		
-		FlxTween.tween(blackBarsLeft, {x: 0, alpha: 1}, 0.5, {ease: FlxEase.quartOut});
-		FlxTween.tween(blackBarsRight, {x: FlxG.width - blackBarsRight.width, alpha: 1}, 0.5, {ease: FlxEase.quartOut});
+		var selectedItem:Alphabet = grpSongs.members[curSelected];
+		if (selectedItem != null && selectedItem.exists)
+		{
+			FlxTween.tween(selectedItem, {alpha: 0}, 0.5, {
+				ease: FlxEase.quartIn,
+				startDelay: 0.2
+			});
+		}
 		
-		zoomCamera = true;
-		FlxG.camera.zoom = 1;
+		var selectedIcon:HealthIcon = iconArray[curSelected];
+		if (selectedIcon != null && selectedIcon.exists)
+		{
+			FlxTween.tween(selectedIcon, {alpha: 0}, 0.5, {
+				ease: FlxEase.quartIn,
+				startDelay: 0.2
+			});
+		}
 		
-		FlxTween.tween(bg, {alpha: 0}, 0.6, {
-			ease: FlxEase.quartIn,
-			onComplete: function(twn:FlxTween) {
-				LoadingState.loadAndSwitchState(new PlayState());
-				FlxG.sound.music.volume = 0;
-				destroyFreeplayVocals();
-				FlxG.camera.zoom = 1;
-				#if (MODS_ALLOWED && DISCORD_ALLOWED)
-				DiscordClient.loadModRPC();
-				#end
-			}
-		});
+		if (bg != null && bg.exists)
+		{
+			FlxTween.tween(bg, {alpha: 0}, 0.6, {
+				ease: FlxEase.quartIn,
+				onComplete: function(twn:FlxTween) {
+					LoadingState.loadAndSwitchState(new PlayState());
+					
+					FlxG.sound.music.volume = 0;
+					destroyFreeplayVocals();
+					#if (MODS_ALLOWED && DISCORD_ALLOWED)
+					DiscordClient.loadModRPC();
+					#end
+				}
+			});
+		}
+		else
+		{
+			LoadingState.loadAndSwitchState(new PlayState());
+			FlxG.sound.music.volume = 0;
+			destroyFreeplayVocals();
+			#if (MODS_ALLOWED && DISCORD_ALLOWED)
+			DiscordClient.loadModRPC();
+			#end
+		}
+		
+		if (whiteScreen != null)
+		{
+			FlxTween.tween(whiteScreen, {alpha: 0.3}, 0.2, {
+				ease: FlxEase.quartOut,
+				onComplete: function(twn:FlxTween) {
+					if (whiteScreen != null && whiteScreen.exists)
+					{
+						FlxTween.tween(whiteScreen, {alpha: 0}, 0.3, {
+							ease: FlxEase.quartIn
+						});
+					}
+				}
+			});
+		}
 	}
 
 	public static function destroyFreeplayVocals() {
@@ -597,48 +615,52 @@ class FreeplayState extends MusicBeatState
 		lastDifficultyName = Difficulty.getString(curDifficulty);
 		diffText.text = "";
 
+		positionHighscore();
 		missingText.visible = false;
 		missingTextBG.visible = false;
 	}
 
 	function changeSelection(change:Int = 0, playSound:Bool = true)
 	{
-		if (player.playingMusic)
-			return;
+		   如果(player.playingMusic)if   如果 (player.playingMusic)
+			   返回;return;
 
 		_updateSongLastDifficulty();
-		if(playSound) FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+		如果(playSound) FlxG.sound   声音.play   玩(路径。声音(scrollMenu), 0.4);if   如果(playSound) FlxG.sound   声音.play   玩(Paths.sound   声音('scrollMenu'), 0.4);
 
-		var lastList:Array<String> = Difficulty.list;
-		curSelected += change;
+		var lastList:Array<String> = Difficulty.list   列表;var lastList:Array<String> = Difficulty.list   列表;
+		curSelected += change;   curSelected = change；
 
-		if (curSelected < 0)
-			curSelected = songs.length - 1;
-		if (curSelected >= songs.length)
-			curSelected = 0;
+		   if   如果 （curSelected < 0）if (curSelected < 0)
+			curSelected = songs.curSelected   长度 =歌曲。长度- 1；length - 1;
+		if   如果 （curSelected >= songs.length   长度）if (curSelected >= songs.length   长度)
+			curSelected =    curSelected = 0；0;
 			
-		var newColor:Int = songs[curSelected].color;
-		if(newColor != intendedColor) {
-			if(colorTween != null) {
-				colorTween.cancel();
+		var newColor:Int = songs[curSelected].color   颜色；var newColor:Int = songs[curSelected].color   颜色;
+		如果(newColor != interdedcolor) {if   如果(newColor != intendedColor) {
+			   如果(colorTween != null   零) {if   如果(colorTween != null   零) {
+				colorTween.   colorTween   取消。cancel ();cancel();
 			}
-			intendedColor = newColor;
-			colorTween = FlxTween.color   颜色(bg, 1, bg.color   颜色, intendedColor, {colorTween = FlxTween。颜色（bg, 1, bg）color, interdedcolor, {
-				onComplete: 不完整:函数(twn:FlxTween) {function   函数(twn:FlxTween) {
+			intendedColor = newColor;intendedColor = newColor；
+			colorTween = FlxTween.colorTween   颜色 = FlxTween。颜色（bg, 1, bg）color, interdedcolor, {color(bg, 1, bg.color   颜色, intendedColor, {
+				不完整:函数(twn:FlxTween) {onComplete: function   函数(twn:FlxTween) {
 					colorTween =    =空;null   零;
 				}
 			});
 		}
 
-		for   为 （i in   在 0…iconArray.length）for (i   我 in   在 0...iconArray.length)
+		var bullShit:Int = 0;
+
+		for   为 (i   我 in   在 0...iconArray.length)for   为 （i in   在 0…iconArray.length）
 		{
-			iconArray[i].iconArray   α[我]。alpha = 0.6;alpha = 0.6;
+			iconArray[i].alpha = 0.6;iconArray[我]。alpha = 0.6;
 		}
 
-		iconArray[curSelected].iconArray   α [curSelected]。Alpha = 1；alpha = 1;
+		iconArray[curSelected].alpha = iconArray [curSelected]。Alpha = 1；1;
 
 		for   为 （grpsong .members中的项）for (item   项 in   在 grpSongs.members)
 		{
+			bullShit++;   废话;
 			item.alpha   α = 0.6;
 			如果项目。targetY == curSelected)if   如果 (item.targetY == curSelected)
 				item.alpha   α = 1;
@@ -668,6 +690,14 @@ class FreeplayState extends MusicBeatState
 		songs[curSelected].歌[curSelected]。lastDifficulty = Difficulty.getString(curDifficulty)；lastDifficulty = Difficulty.getString(curDifficulty);
 	}
 
+	私有函数positionHighscore() {private   私人 function   函数 positionHighscore() {
+		scoreText.scoreText。x = FlxG。宽度- scoreText。宽度- 6;x = FlxG.width   宽度 - scoreText.width   宽度 - 6;
+		scoreBG.scoreBG   规模.scale   规模.x = FlxG。宽度- scoreText。x 6;scale.x = FlxG.width   宽度 - scoreText.x + 6;
+		scoreBG.scoreBG。x = FlxG。宽度- （scoreBG.scale   规模.）X / 2)；x = FlxG.width   宽度 - (scoreBG.scale   规模.x / 2);
+		diffText.diffText .x = Std.int（scoreBG.x (scoreBG)）宽度/ 2))；x = Std.int(scoreBG.x + (scoreBG.width   宽度 / 2));
+		diffText.diffText .X -= diffText。宽度/ 2；x -= diffText.width   宽度 / 2;
+	}
+
 	var _drawDistance:Int = 4;
 	var _lastVisibles:Array<Int> = [];var _lastVisibles:Array<Int> = [];
 	公共函数updatetext （elapsed:Float = 0.0   公共）public function   函数 updateTexts(elapsed   运行:Float = 0.0)
@@ -680,20 +710,17 @@ class FreeplayState extends MusicBeatState
 		}
 		_lastVisibles = [];   _lastVisible = []；
 
-		var centerY:Float = FlxG.height   高度 / 2;
 		var min   最小值:Int = Math.round   轮(马克斯(0,Math.min(歌曲。length, lerpSelected - _drawDistance))；var min:Int = Math.round   轮(Math.max   马克斯(0, Math.min(songs.length, lerpSelected - _drawDistance)));
 		var max   马克斯:Int = Math.round   轮马克斯(0,Math.min   最小值(歌曲。长度，lerpSelected _drawDistance)))；var max:Int = Math.round   轮(Math.max   马克斯(0, Math.min(songs.length, lerpSelected + _drawDistance)));
 		   For （i in   为 min…max）for (i in   在 min...max)
 		{
 			var item   项：字母表= grpsong .members   成员[i]；var item:Alphabet = grpSongs.members   成员[i];
 			item.项   可见。可见=项目。Active = true   真正的；visible = item.active   活跃的 = true   真正的;
-			item.项。x = FlxG。宽度/ 2 -项目。宽度/ 2；x = FlxG.width   宽度 / 2 - item.width   宽度 / 2;
-			item.项。y = centerY （(item.)）targetY - lerpSelected) * 80)；y = centerY + ((item.targetY - lerpSelected) * 80);
-			
+			item.项。X =（）项。targetY - lerpSelected) * item.distancePerItem。x) item.startPosition.x;x = ((item.targetY - lerpSelected) * item.distancePerItem.x) + item.startPosition.x;
+			item.项。Y =（）项。target - lerpSelected) * 1.3 * item.distancePerItem。y) item.startPosition.y;y = ((item.targetY - lerpSelected) * 1.3 * item.distancePerItem.y) + item.startPosition.y;
+
 			var icon   图标:HealthIcon = conarray [i];var icon   图标:HealthIcon = iconArray[i];
 			icon.图   可见标。可见=图标。Active = true   真正的；visible = icon.active   活跃的 = true   真正的;
-			icon.图标。X =物品。x项目。宽度/ 2 -图标。宽度/ 2；x = item.x + item.width   宽度 / 2 - icon.width   宽度 / 2;
-			icon.图标。Y =物品。y 60;y = item.y + 60;
 			_lastVisibles.   _lastVisibles   推.push   推(我);push(i);
 		}
 	}
@@ -710,15 +737,15 @@ class FreeplayState extends MusicBeatState
 
 class   类   类SongMetadata SongMetadata
 {
-	public   公共 var songName:String = "；public var songName:String = "";   ""
-	public var week:Int = 0；public var week:Int = 0;
-	public var songCharacter:String = "   公共；public var songCharacter:String = "";
-	public   公共 var color   颜色:Int = -7179779；public var color   颜色:Int = -7179779;
-	public   公共 var folder   文件夹:String = "；public var folder:String = "";
+	public   公共 var songName:String = "；public var songName:String = "";   "   “;"
+	public var week:Int = 0；public var week:Int = 0;public var week:Int = 0；
+	public var songCharacter:String = "   公共；public var songCharacter:String = "";   “;
+	publicpublic var   公   颜色共 color:Int = -7179779； var color   颜色:Int = -7179779；public var color   颜色:Int = -7179779;
+	public   公共 var folder   文件夹:String = "；public var folder:String = "";   “;
 	公共var last难度：字符串= null；public var lastDifficulty:String = null;
 
-	public function new（song:String, week:Int, song character:String, color:Int）public function new(song:String, week:Int, songCharacter:String, color:Int)
-	{   公共
+	public function new（song:String, week:Int, song character:String, color:Int）public function new(song:String, week:Int, songCharacter:String, color:Int)public function new（song:String, week:Int, song character:String, color:Int）
+	{public function new（song:String, week:Int, song character:String, color:Int）   公共
 		   这一点。songName =歌曲；this.songName = song;
 		   这一点。Week =周；this.week = week;
 		这一点。songCharacter = songCharacter；this.songCharacter = songCharacter;
@@ -726,4 +753,4 @@ class   类   类SongMetadata SongMetadata
 		this.folder = Mods.currentModDirectory;
 		如果这一点。文件夹== null)这个。文件夹= "   文件夹；if(this.folder   文件夹 == null   零) this.folder   文件夹 = '';
 	}
-}
+		}
